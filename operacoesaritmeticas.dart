5a4c0737de665ebe94485ac260ec1f711c06ae54
Jeffter Mayron Ferreira Olírio <jefftermayron@gmail.com>
2023-02-07 14:37:13 -0300

    Create operacoesaritmeticas.dart
    Operações em Dart

---
+++ operacoesaritmeticas.dart
@@ -0,0 +1,18 @@
+void main() {
+  double n1 = 10.5;
+  double n2 = 47.9;
+  int n3 = 3;
+  
+  double r1 = n3 * (n1 + n2 - 15) * 10;
+  print(r1); //operações
+  
+  n1 += 10; // anotação simplificada
+  /* n1 -= 10;
+  n1 *= 10;
+  n1 /= 10; */
+  
+  n3++; //incremento
+  n3--; //decremento
+  
+  print(n3 % 2); //resto
+}
