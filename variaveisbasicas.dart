d5b83297795f0dfe6a79e3ceeae221b4eec30e7d
Jeffter Mayron Ferreira Olírio <jefftermayron@gmail.com>
2023-02-07 13:48:58 -0300

    Create variaveisbasicas.dart
    Variáveis Básicas do Dart.

---
+++ variaveisbasicas.dart
@@ -0,0 +1,12 @@
+void main() {
+  int idade = 18;
+  String nome = 'Jeffter';
+  double distancia = 99.5;
+  print('$nome você tem $idade anos');
+  print(distancia);
+  
+  idade = 19;
+  nome = 'Jeffter Mayron';
+  
+  print('$nome você tem $idade anos');
+}
