2a1152d23cd8f07a69abd86297b93ab6669d7abd
Jeffter Mayron Ferreira Olírio <jefftermayron@gmail.com>
2023-02-08 23:31:54 -0300

    Create operadorternario.dart

---
+++ operadorternario.dart
@@ -0,0 +1,8 @@
+void main() {
+  double nota = 4;
+  String mensagem = nota >= 6 ? 'Aprovado' : 'Reprovado' ;
+  // int numero = nota == 5 ? 10 : 2;
+  // como um if/else dentro de uma variável (exemplo)
+  print (mensagem);
+
+}
