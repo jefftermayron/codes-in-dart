26d619386024e733091de6003e7650a423235f89
Jeffter Mayron Ferreira Olírio <jefftermayron@gmail.com>
2023-02-08 23:42:21 -0300

    Create interruptor.dart

---
+++ interruptor.dart
@@ -0,0 +1,17 @@
+void main() {
+  String linguagem = 'Dart';
+  
+  switch(linguagem) {
+    case 'Dart':
+      print('Linguagem de alto nivel');
+      break;
+    case 'Java':
+      print('Liguagem de alto nivel');
+      break;
+    case 'Assembly':
+      print('Linguagem de baixo nivel');
+      break;
+    default:
+      print('Desconhecida');
+  }
+}
