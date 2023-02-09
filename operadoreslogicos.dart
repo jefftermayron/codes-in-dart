fb20cb287a06b46394269b1b27c81599d44195d6
Jeffter Mayron Ferreira Olírio <jefftermayron@gmail.com>
2023-02-08 23:06:51 -0300

    Create operadoreslogicos.dart

---
+++ operadoreslogicos.dart
@@ -0,0 +1,14 @@
+void main() {
+  int n = 10;
+  int n2 = 15;
+  bool r = n == 10 || n == n2; // igual
+  /* bool r = n != 10; (diferente)
+  bool r = n > 10; (maior)
+  bool r = n < 10; (menor)
+  bool r = n >= 10; (maior igual)
+  bool r = n <= 10; (menor igual) */
+  print(r);
+  //print(!r); inverte resultado (not)
+  
+  // and( && ) or( || ) 
+}
